162554af21314c86943713f85f54765dce4dbfe2
Joshuauzor <joshuauzor10@gmail.com>
2020-05-06 14:35:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+document.querySelector('.switch').addEventListener('click', function() {
+    document.querySelector('body').style.background = 'White';
+    document.querySelector('body').style.color = 'black';
+    document.querySelector('.upper-col-1').style.background = 'whitesmoke';
+    document.querySelector('.upper-col-2').style.background = 'whitesmoke';
+    document.querySelector('.upper-col-3').style.background = 'whitesmoke';
+    document.querySelector('.upper-col-4').style.background = 'whitesmoke';
+    document.querySelector('h5').style.color = 'black';
+    document.querySelector('.lower-col-1').style.background = 'whitesmoke';
+    document.querySelector('.lower-col-2').style.background = 'whitesmoke';
+    document.querySelector('.lower-col-3').style.background = 'whitesmoke';
+    document.querySelector('.lower-col-4').style.background = 'whitesmoke';
+    document.querySelector('.lower-col-1').style.color = 'black';
+    document.querySelector('.lower-col-2').style.color = 'black';
+    document.querySelector('.lower-col-3').style.color = 'black';
+    document.querySelector('.lower-col-4').style.color = 'black';
+
+    // lowest section
+    document.querySelector('.lowest-col-1').style.background = 'whitesmoke';
+    document.querySelector('.lowest-col-2').style.background = 'whitesmoke';
+    document.querySelector('.lowest-col-3').style.background = 'whitesmoke';
+    document.querySelector('.lowest-col-4').style.background = 'whitesmoke';
+    document.querySelector('.lowest-col-1').style.color = 'black';
+    document.querySelector('.lowest-col-2').style.color = 'black';
+    document.querySelector('.lowest-col-3').style.color = 'black';
+    document.querySelector('.lowest-col-4').style.color = 'black';
+})
